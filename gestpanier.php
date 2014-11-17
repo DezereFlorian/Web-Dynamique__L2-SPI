c809869b782e5f40f37b702d897695f85de89dfa
Chouwibaka <Valent72-1706@hotmail.fr>
2014-11-17 19:02:08 +0100

    gestion du panier terminée

--- gestpanier.php
+++ gestpanier.php
@@ -11,16 +11,28 @@ $idjeu = $_POST["id"];
 if (isset($_SESSION['id'])): 
     if ($_POST['status']==1):
         if (sizeof($_SESSION['panier'])<3):
-            if(array_search($idjeu, $_SESSION['panier'])==false):
+            if(in_array($idjeu, $_SESSION['panier'])==true):
+                echo "<script>alert('Le jeu est déjà dans le panier');</script>";
+            else:
                 $_SESSION['panier'][] = $idjeu;
-            endif;     
-        else:
-            echo "<script>if(confirm('Votre panier est plein, vous ne pouvez pas ajouter plus de 3 jeux à votre commande.\nAppuyez sur Ok pour \u00eatre redirig\u00e9 vers votre panier, Annuler pour rester sur cette page.')){var adresse = 'panier.php'; window.location = adresse;};</script>";
-        endif;
+                echo "<script>alert('Le jeu a bien été ajouté !');</script>";
+            endif;   
+        else:?>
+        <script>
+            if(confirm('Votre panier est plein, vous ne pouvez pas ajouter plus de 3 jeux à votre commande.\nAppuyez sur Ok pour \u00eatre redirig\u00e9 vers votre panier, Annuler pour rester sur cette page.'))
+            {
+                var adresse = 'panier.php'; 
+                window.location = adresse;
+            }
+        </script>
+        <?php endif;
     else:
         $idjeuretire = array_search($idjeu, $_SESSION['panier']);
         unset($_SESSION['panier'][$idjeuretire]);
         $_SESSION['panier'] = array_values($_SESSION['panier']);
+        echo "<script>alert('Le jeu a bien été retiré !');
+                var adresse = 'panier.php';
+                window.location = adresse;</script>";
     endif;
 else:
     ?>
--- panier.php
+++ panier.php
@@ -1,6 +1,14 @@
 <?php
     include_once('entete.php');
     
+    function dateFR($datePHP1) // transforme la date anglaise (de la base) en date française
+    {
+        list($AAAA, $MM, $JJ) = explode("-", $datePHP1);
+        $datePHP2 = $JJ."-".$MM."-".$AAAA;
+
+        return $datePHP2;
+    }
+    
     function get_panier($nb, $articles){
         global $bdd;
         switch($nb):
@@ -8,10 +16,10 @@
                 $req = $bdd -> prepare("Select * from jeux where IDJeux='$articles[0]'");
                 break;
             case 2:
-                $req = $bdd -> prepare("Select * from jeux where IDJeux='$articles[0] or IDJeux='$articles[1]'");
+                $req = $bdd -> prepare("Select * from jeux where IDJeux='$articles[0]' or IDJeux='$articles[1]'");
                 break;
             case 3:
-                $req = $bdd -> prepare("Select * from jeux where IDJeux='$articles[0] or IDJeux='$articles[1]' or IDJeux='$articles[2]'");
+                $req = $bdd -> prepare("Select * from jeux where IDJeux='$articles[0]' or IDJeux='$articles[1]' or IDJeux='$articles[2]'");
                 break;
         endswitch;
         $req -> execute();
@@ -20,40 +28,55 @@
     }
     
     $nb_articles = sizeof($_SESSION['panier']);
-    $lstPanier = get_panier($nb_articles, $_SESSION['panier']);
     
+if($nb_articles == 0): ?>
+    <p>Votre panier est vide.</p>
+<?php else:
+    $lstPanier = get_panier($nb_articles, $_SESSION['panier']);
+?>
+    <table>
+<?php     
     foreach($lstPanier as $unJeu):
         $id = $unJeu['IDJeux'];
-            $nom = $unJeu['NomJeu'];
-            $desc = $unJeu['Descriptif'];
-            $prix = $unJeu['Prix'];
-            $stock = $unJeu['Stock'];
-            $img = "images/".$unJeu['Image'];
-            $sortie = dateFR($unJeu['DateDeSortie']);
-            $categ = $unJeu['NomCateg'];
-            $age = $unJeu['TrancheAge'];
-// affichage de chaque jeu
-    ?>
-        <tr>
-            <td><span class="cache"><?php echo $id; ?></span><a class="liendetail" href="detail_jeu.php?id=<?php echo $id; ?>"><?php echo $nom; ?></a></td> <!-- mets le span en caché avec visibility : hidden -->
-            <td><a class="" href="detail_jeu.php?id=<?php echo $id; ?>"><img src="<?php echo $img; ?>"/></a></td>
-            <td class='contenu_case'><?php echo $desc; ?></td>
-            <td class='contenu_case'><?php echo $sortie; ?></td>
-            <td class='contenu_case'><?php echo $prix; ?></td>
-            <td class='contenu_case'><?php echo $categ; ?></td>
-            <td class='contenu_case'><?php echo $age; ?></td>
-            <td class='cachepanier'>
-                <?php if($stock > 0): ?>
-                    <img class='retirepanier' id="<?php echo $id ?>" src='images/panier_fleche.jpg'/>
-                <?php else: ?>
-                    Indisponible
-                <?php endif; ?>
-            </td> <!-- ajout au panier via jQuery -->
-        </tr>
-    <?php
-    endforeach;
+        $nom = $unJeu['NomJeu'];
+        $desc = $unJeu['Descriptif'];
+        $prix = $unJeu['Prix'];
+        $img = "images/".$unJeu['Image'];
+        $sortie = dateFR($unJeu['DateDeSortie']);
+        // affichage de chaque jeu du panier
 ?>
-    
-<?php
+    <tr>
+        <td><span class="cache"><?php echo $id; ?></span><a class="liendetail" href="detail_jeu.php?id=<?php echo $id; ?>"><?php echo $nom; ?></a></td> <!-- mets le span en caché avec visibility : hidden -->
+        <td><a class="" href="detail_jeu.php?id=<?php echo $id; ?>"><img src="<?php echo $img; ?>"/></a></td>
+        <td class='contenu_case'><?php echo $desc; ?></td>
+        <td class='contenu_case'><?php echo $sortie; ?></td>
+        <td class='contenu_case'><?php echo $prix; ?></td>
+        <td class='cachepanier'>
+                <img class='retirepanier' id="<?php echo $id ?>" src='images/retirer_panier.jpg'/>
+        </td> <!-- enlève du panier via jQuery -->
+    </tr>
+<?php endforeach; ?>
+    </table>    
+<?php endif;
     include_once('foot.php');
 ?>
+
+<script>
+    $(".retirepanier").click(function (){
+        var id = $(this).attr('id');
+        $.ajax({
+                  type:'POST',
+                  url:'gestpanier.php',
+                  data:{
+                      status : 2,
+                      id : id
+                  },
+                  success: function(data,textStatus,jqXHR){
+                        $(data).prependTo('table');
+                  },
+                  error: function(jqXHR, textStatus,errorThrown){
+                      alert('une erreur s\'est produite');
+                  }
+                });
+    });
+</script>
